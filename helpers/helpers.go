b20a31ffcef7eaad36fe2605c7825845757ceaf0
Guido Scalise <guido.scalise@gmail.com>
2024-12-18 09:48:11 +0000

    Day 18

--- helpers/helpers.go
+++ helpers/helpers.go
@@ -22,3 +22,42 @@ func ParseInputDay01(input string) ([]int, []int) {
 	}
 	return left, right
 }
+
+type Direction struct {
+	DX, DY int
+}
+
+var UP Direction = Direction{0, -1}
+var DOWN Direction = Direction{0, 1}
+var LEFT Direction = Direction{-1, 0}
+var RIGHT Direction = Direction{1, 0}
+
+var Directions [4]*Direction = [4]*Direction{&DOWN, &LEFT, &UP, &RIGHT}
+
+type Position struct {
+	X, Y int
+}
+
+var ZERO_ZERO = Position{X: 0, Y: 0}
+
+func (p Position) Displace(m Direction) Position {
+	return Position{
+		X: p.X + m.DX,
+		Y: p.Y + m.DY,
+	}
+}
+
+func (d Direction) Opposite() Direction {
+	switch d {
+	case UP:
+		return DOWN
+	case DOWN:
+		return UP
+	case RIGHT:
+		return LEFT
+	case LEFT:
+		return RIGHT
+	default:
+		panic("invalid")
+	}
+}
